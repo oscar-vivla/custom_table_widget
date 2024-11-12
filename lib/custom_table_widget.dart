733c8240eda9c7b3c05b94879853c2c323f551c4
Oscar Sangrador <oscar@vivla.com>
2024-11-12 16:52:54 +0100

    remove default package class

--- lib/custom_table_widget.dart
+++ lib/custom_table_widget.dart
@@ -1,9 +1,3 @@
 library custom_table_widget;
 
 export './table_widget_wrapper.dart';
-
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
