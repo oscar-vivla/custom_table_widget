0a5866581c54ebd90314968072fbb07d438d491c
Oscar Sangrador <oscar@vivla.com>
2024-11-19 12:12:09 +0100

    add name columns by default and accept widgets like value

--- lib/table_widget_wrapper.dart
+++ lib/table_widget_wrapper.dart
@@ -1,119 +1,98 @@
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
 import 'package:syncfusion_flutter_datagrid/datagrid.dart';
 
-class GenericDataGridSource<T> extends DataGridSource {
+List<String> columnOrder = [
+  'Id reserva',
+  'Casa',
+  'Propietario',
+  'Fecha de reserva',
+  'Tipo de reserva',
+  'Estado de la estancia',
+  'Huésped principal',
+  'Notas huéspedes',
+  'Fecha de entrada',
+  'Fecha de salida',
+  'Nº de personas',
+  'Nº de estancias',
+  'Nº de locker',
+  'CX',
+  'Planificación',
+  'Check in',
+  'Pre-estancia',
+  'Control de llegada',
+  'NPS',
+  'Disfrutada',
+  'Notas',
+  'Plan de viaje',
+];
+
+class DynamicDataGrid<T> extends StatelessWidget {
   final List<T> data;
-  final List<String> columnNames;
+  final List<String> columnNames = columnOrder;
+  final List<Widget Function(T)> columnGetters;
 
-  GenericDataGridSource({
-    required this.data,
-    required this.columnNames,
-  });
-
-  @override
-  List<DataGridRow> get rows => data.map<DataGridRow>((item) {
-        return DataGridRow(
-          cells: columnNames.map<DataGridCell>((columnName) {
-            return DataGridCell(
-              columnName: columnName,
-              value: _getValue(item, columnName),
-            );
-          }).toList(),
-        );
-      }).toList();
-
-  @override
-  DataGridRowAdapter buildRow(DataGridRow row) {
-    return DataGridRowAdapter(
-      cells: row.getCells().map<Widget>((cell) {
-        if (cell.value is Widget) {
-          return cell.value as Widget;
-        } else if (cell.value is num) {
-          return Container(
-            alignment: Alignment.centerRight,
-            padding: const EdgeInsets.symmetric(horizontal: 8),
-            child: Text(cell.value.toString()),
-          );
-        } else if (cell.value is String) {
-          return Container(
-            alignment: Alignment.centerLeft,
-            padding: const EdgeInsets.symmetric(horizontal: 8),
-            child: Text(cell.value),
-          );
-        } else if (cell.value is bool) {
-          return Checkbox(
-            value: cell.value as bool,
-            onChanged: null,
-          );
-        } else {
-          return Container(
-            alignment: Alignment.center,
-            padding: const EdgeInsets.symmetric(horizontal: 8),
-            child: Text(cell.value.toString()),
-          );
-        }
-      }).toList(),
-    );
-  }
-
-  dynamic _getValue(T item, String columnName) {
-    try {
-      final value = (item as Map)[columnName];
-      return value;
-    } catch (_) {
-      throw Exception(
-          "El objeto debe ser un Map o tener un método para obtener el valor de '$columnName'");
-    }
-  }
-}
-
-class CustomDataGrid<T> extends StatelessWidget {
-  final List<T> data;
-  final List<GridColumn> columns;
-
-  const CustomDataGrid({
+  DynamicDataGrid({
     super.key,
     required this.data,
-    required this.columns,
+    required this.columnGetters,
   });
 
   @override
   Widget build(BuildContext context) {
-    final List<String> columnNames = [
-      'Id reserva',
-      'Casa',
-      'Propietario',
-      'Fecha de reserva',
-      'Tipo de reserva',
-      'Estado de la estancia',
-      'Huésped principal',
-      'Notas huéspedes',
-      'Fecha de entrada',
-      'Fecha de salida',
-      'Nº de personas',
-      'Nº de estancias',
-      'Nº de locker',
-      'CX',
-      'Planificación',
-      'Check in',
-      'Pre-estancia',
-      'Control de llegada',
-      'NPS',
-      'Disfrutada',
-      'Notas',
-      'Plan de viaje',
-    ];
     return SfDataGrid(
-      source: GenericDataGridSource<T>(
-        data: data,
-        columnNames: columnNames,
-      ),
-      columns: columns,
-      frozenColumnsCount: 4,
-      columnWidthMode: ColumnWidthMode.auto,
-      allowColumnsDragging: true,
-      showCheckboxColumn: true,
+      source: DynamicDataGridSource<T>(data, columnGetters),
+      columns: _getColumns(),
+    );
+  }
+
+  // Generación de columnas dinámicas según los nombres de columna
+  List<GridColumn> _getColumns() {
+    return columnNames
+        .map((name) => GridColumn(
+              columnName: name,
+              label: Text(name, style: const TextStyle(color: Colors.grey)),
+            ))
+        .toList();
+  }
+}
+
+class DynamicDataGridSource<T> extends DataGridSource {
+  final List<T> data;
+  final List<Widget Function(T)> columnGetters;
+
+  DynamicDataGridSource(this.data, this.columnGetters);
+
+  @override
+  List<DataGridRow> get rows {
+    return data.map((item) {
+      return DataGridRow(
+        cells: columnGetters
+            .asMap()
+            .map((index, getter) {
+              return MapEntry(
+                index,
+                DataGridCell<Widget>(
+                  columnName: columnOrder[index],
+                  value: getter(item),
+                ),
+              );
+            })
+            .values
+            .toList(),
+      );
+    }).toList();
+  }
+
+  @override
+  DataGridRowAdapter buildRow(DataGridRow row) {
+    return DataGridRowAdapter(
+      cells: row.getCells().map<Widget>((dataGridCell) {
+        return Container(
+          padding: const EdgeInsets.all(8.0),
+          alignment: Alignment.center,
+          child: dataGridCell.value,
+        );
+      }).toList(),
     );
   }
 }
