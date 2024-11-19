e82c498916e9457c3633617c61986f7c05c9d368
Oscar Sangrador <oscar@vivla.com>
2024-11-19 09:20:13 +0100

    add more datagrid config

--- lib/table_widget_wrapper.dart
+++ lib/table_widget_wrapper.dart
@@ -27,11 +27,32 @@ class GenericDataGridSource<T> extends DataGridSource {
   DataGridRowAdapter buildRow(DataGridRow row) {
     return DataGridRowAdapter(
       cells: row.getCells().map<Widget>((cell) {
-        return Container(
-          alignment: Alignment.centerLeft,
-          padding: const EdgeInsets.symmetric(horizontal: 8),
-          child: Text(cell.value.toString()),
-        );
+        if (cell.value is Widget) {
+          return cell.value as Widget;
+        } else if (cell.value is num) {
+          return Container(
+            alignment: Alignment.centerRight,
+            padding: const EdgeInsets.symmetric(horizontal: 8),
+            child: Text(cell.value.toString()),
+          );
+        } else if (cell.value is String) {
+          return Container(
+            alignment: Alignment.centerLeft,
+            padding: const EdgeInsets.symmetric(horizontal: 8),
+            child: Text(cell.value),
+          );
+        } else if (cell.value is bool) {
+          return Checkbox(
+            value: cell.value as bool,
+            onChanged: null,
+          );
+        } else {
+          return Container(
+            alignment: Alignment.center,
+            padding: const EdgeInsets.symmetric(horizontal: 8),
+            child: Text(cell.value.toString()),
+          );
+        }
       }).toList(),
     );
   }
@@ -50,23 +71,49 @@ class GenericDataGridSource<T> extends DataGridSource {
 class CustomDataGrid<T> extends StatelessWidget {
   final List<T> data;
   final List<GridColumn> columns;
-  final List<String> columnNames;
 
   const CustomDataGrid({
     super.key,
     required this.data,
     required this.columns,
-    required this.columnNames,
   });
 
   @override
   Widget build(BuildContext context) {
+    final List<String> columnNames = [
+      'Id reserva',
+      'Casa',
+      'Propietario',
+      'Fecha de reserva',
+      'Tipo de reserva',
+      'Estado de la estancia',
+      'Huésped principal',
+      'Notas huéspedes',
+      'Fecha de entrada',
+      'Fecha de salida',
+      'Nº de personas',
+      'Nº de estancias',
+      'Nº de locker',
+      'CX',
+      'Planificación',
+      'Check in',
+      'Pre-estancia',
+      'Control de llegada',
+      'NPS',
+      'Disfrutada',
+      'Notas',
+      'Plan de viaje',
+    ];
     return SfDataGrid(
       source: GenericDataGridSource<T>(
         data: data,
         columnNames: columnNames,
       ),
       columns: columns,
+      frozenColumnsCount: 4,
+      columnWidthMode: ColumnWidthMode.auto,
+      allowColumnsDragging: true,
+      showCheckboxColumn: true,
     );
   }
 }
